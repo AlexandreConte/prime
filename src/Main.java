d6fd32cf939db3458a728eda954100b026f66ff0
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-07-16 09:55:44 -0300

    add main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero para exibir os numeros primos até ele: ");
+        int upperLimit;
+        try {
+            upperLimit = sc.nextInt();
+            List<Integer> numbers = PrimeNumber.primeNumbers(upperLimit);
+            System.out.println(numbers);
+        } catch (InputMismatchException exception) {
+            System.out.println("Digite um numero inteiro!");
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        sc.close();
     }
 }
